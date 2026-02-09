002d17038f967836e34e7478ed9deae067175eac
Sameer Watts <sameerwatts00@gmail.com>
2026-02-09 19:04:47 +0530

    Classes in typescript

--- typescript.ts
+++ typescript.ts
@@ -88,3 +88,37 @@ let person = {
 
 fullName(person);
 
+
+// classes
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good morning ${this.employeeName}`)
+    }
+}
+
+let emp1 = new Employee('Sameer');
+
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+
+    delegateWork() {
+        console.log(`Manager delegating tasks`)
+    }
+}
+
+let m1 = new Manager('Kunal')
+
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
