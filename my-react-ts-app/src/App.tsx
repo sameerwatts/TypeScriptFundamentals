959a3d8f8e50eb9622401c9b6c8da2929ba3f04a
Sameer Watts <sameerwatts00@gmail.com>
2026-02-10 19:23:24 +0530

    Cleanup

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,16 +1,7 @@
 import "./App.css";
-import Container from "./components/Container";
-import GreetWithOptionalMessage from "./components/GreetWithOptionalMessage";
-import Person from "./components/Person";
 
 function App() {
-  return (
-    <div id="root">
-      <Person name={{first: 'Sameer', last: 'Watts'}} />
-      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
-      <GreetWithOptionalMessage isLoggedIn={true} name="Sam" />
-    </div>
-  );
+  return <div id="root"></div>;
 }
 
 export default App;
