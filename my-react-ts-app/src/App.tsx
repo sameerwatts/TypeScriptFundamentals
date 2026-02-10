56380194d8aaed7a0d79f31141d98f1e9b7ece00
Sameer Watts <sameerwatts00@gmail.com>
2026-02-10 14:22:48 +0530

    Code cleanup

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,19 +1,7 @@
 import "./App.css";
-import Button from "./components/Button";
-import Input from "./components/Input";
 
 function App() {
-  return (
-    <div id="root">
-      <Button
-        handleClick={(e, id) => {
-          console.log("button clicked", e);
-          console.log("button clicked id", id);
-        }}
-      />
-      <Input value="" handleChange={(e) => console.log(e)} />
-    </div>
-  );
+  return <div id="root"></div>;
 }
 
 export default App;
