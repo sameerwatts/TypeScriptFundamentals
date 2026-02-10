47f1bf98d933db45f23011957e819e0ac2551a47
Sameer Watts <sameerwatts00@gmail.com>
2026-02-10 20:09:06 +0530

    typr checking in useReducer

---
+++ my-react-ts-app/src/components/state/Counter.tsx
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+
+type CounterStateProps = {
+  count: number;
+};
+
+type CounterActionProps = {
+  type: string;
+  payload: number;
+};
+
+const initialState = { count: 0 };
+
+function reducer(state: CounterStateProps, action: CounterActionProps) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+    case "decrement":
+      return { count: state.count - action.payload };
+    default:
+      return state;
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment", payload: 10 })}>
+        Increament by 10
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
+        Decrement by 10
+      </button>
+    </>
+  );
+};
+
+export default Counter;
