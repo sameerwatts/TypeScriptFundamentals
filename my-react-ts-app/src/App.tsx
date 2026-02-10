f64cfa586a25ef39067f71fad86479d3cc7d11a4
Sameer Watts <sameerwatts00@gmail.com>
2026-02-10 12:26:28 +0530

    Button click event props

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-
   return (
     <div id="root">
-      
+      <Button
+        handleClick={(e) => {
+          console.log("button clicked", e);
+        }}
+      />
     </div>
   );
 }
