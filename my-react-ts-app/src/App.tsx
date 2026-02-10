91fca1dd4496b2698eef7d90985b2787125b4545
Sameer Watts <sameerwatts00@gmail.com>
2026-02-10 22:51:52 +0530

    useContext with typescript

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Box from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+
 
 function App() {
-  return <div id="root"></div>;
+  return <div id="root">
+    <ThemeContextProvider><Box/></ThemeContextProvider>
+  </div>;
 }
 
 export default App;
