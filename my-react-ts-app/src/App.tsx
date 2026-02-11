a1783adbd09b89133ec12fc624a932ec81a39b85
Sameer Watts <sameerwatts00@gmail.com>
2026-02-11 12:39:33 +0530

    cleanup

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import User from "./components/context/User";
-import { UserContextProvider } from "./components/context/Usercontext";
-// import Box from "./components/context/Box";
-// import { ThemeContextProvider } from "./components/context/ThemeContext";
-
 
 function App() {
-  return <div id="root">
-    {/* <ThemeContextProvider><Box/></ThemeContextProvider> */}
-    <UserContextProvider>
-      <User />
-    </UserContextProvider>
-  </div>;
+  return <div id="root"></div>;
 }
 
 export default App;
