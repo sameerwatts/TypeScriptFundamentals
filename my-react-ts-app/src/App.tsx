13022d789b6b2f658c6a59deacd7a2711de897a9
Sameer Watts <sameerwatts00@gmail.com>
2026-02-12 10:14:00 +0530

    Mutable refs in typescript

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import MutableRef from "./components/ref/MutableRef";
 
 function App() {
-  return <div id="root"></div>;
+  return <div id="root">
+    <MutableRef />
+  </div>;
 }
 
 export default App;
