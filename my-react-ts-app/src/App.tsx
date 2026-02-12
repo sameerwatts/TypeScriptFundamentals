e0abf37d39231cbb78eaa913faf3f15513e516cc
Sameer Watts <sameerwatts00@gmail.com>
2026-02-12 23:09:15 +0530

    Cleanup

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,36 +1,7 @@
 import "./App.css";
-import Private from "./components/auth/Private";
-import Profile from "./components/auth/Profile";
-import List from "./components/genetics/List";
-import RandomNumber from "./components/restrictions/RandomNumber";
-// import MutableRef from "./components/ref/MutableRef";
 
 function App() {
-  return (
-    <div id="root">
-      {/* <MutableRef /> */}
-      {/* <Private isLoggedIn={true} Component={Profile}/> */}
-      {/* <List
-        items={["Batman", "Superman", "Wonder Women"]}
-        handleListItemClick={(item) => console.log(item)}
-        renderItem={(user) => user}
-        />
-      <List
-        items={[1, 2, 3]}
-        handleListItemClick={(item) => console.log(item)}
-        renderItem={(user) => user}
-      />
-      <List
-        items={[
-          { id: 1, first: "Sameer", last: "Watts" },
-          { id: 2, first: "Paru", last: "Kukkar" },
-        ]}
-        handleListItemClick={(item) => console.log(item)}
-        renderItem={(user) => `${user.first} ${user.last}`}
-      /> */}
-      <RandomNumber value={10} isNegative/>
-    </div>
-  );
+  return <div id="root"></div>;
 }
 
 export default App;
