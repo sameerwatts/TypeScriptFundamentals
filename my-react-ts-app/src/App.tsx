2eefc5460d90e633cf33a73d0086fc21c4bd37c3
Sameer Watts <sameerwatts00@gmail.com>
2026-02-13 10:58:40 +0530

    Cleanup

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import CustomButton from "./components/html/CustomButton";
-import CustomInput from "./components/html/CustomInput";
 
 function App() {
-  return (
-    <div id="root">
-      <CustomButton
-        variant="primary"
-        onClick={() => {
-          console.log("clicked");
-        }}
-      >
-       {/* <div>Label</div> */}
-       label
-      </CustomButton>
-      <CustomInput type="text" value={20}/>
-    </div>
-  );
+  return <div id="root"></div>;
 }
 
 export default App;
