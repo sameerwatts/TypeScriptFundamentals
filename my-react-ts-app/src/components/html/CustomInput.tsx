cb91daf2707ee09456b641bdeef545c4765669f4
Sameer Watts <sameerwatts00@gmail.com>
2026-02-13 10:57:57 +0530

    Wrapping HTML element

---
+++ my-react-ts-app/src/components/html/CustomInput.tsx
@@ -0,0 +1,10 @@
+type InputProps = React.ComponentProps<'input'>
+const CustomInput = (props: InputProps) => {
+    return (
+        <div>
+            <input {...props} />
+        </div>
+    );
+};
+
+export default CustomInput;
